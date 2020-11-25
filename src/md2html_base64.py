6e4866901bdb5627b65017fb161c2b0aa0924a15
likianta <sheerish@qq.com>
2020-11-26 01:45:08 +0800

    Add stylesheet to html

--- src/md2html_base64.py
+++ src/md2html_base64.py
@@ -1,6 +1,8 @@
 import re
 from os.path import abspath, split, splitext
 
+from lk_utils.read_and_write import read_file
+
 from common import encode_img, get_img_path
 
 
@@ -31,7 +33,12 @@ def main(ifile: str, ofile=''):
     if ofile == '':
         ofile = splitext(ifile)[0] + '.html'
     with open(ofile, 'w', encoding='utf-8') as f:
-        f.write(compose_html(doc, enable_checkbox=True))
+        f.write(
+            compose_html(
+                title=splitext(fname)[0],
+                doc=doc,
+            )
+        )
     print('See output at "{}:0"'.format(ofile))
 
 
@@ -59,31 +66,75 @@ def fetch_image_links(doc: str) -> dict:
     return out
 
 
-def compose_html(doc, enable_checkbox=False):
+def compose_html(title, doc):
     import markdown2
     html = markdown2.markdown(doc, extras={
         # https://github.com/trentm/python-markdown2/wiki/Extras
-        'code-friendly': None, 'cuddled-lists': None,
+        'code-friendly'     : None, 'cuddled-lists': None,
         'fenced-code-blocks': None, 'header-ids': None, 'numbering': None,
-        'strike': None, 'tables': None, 'task_list': None, 'toc': None,
-        'html-classes': {
+        'strike'            : None, 'tables': None, 'task_list': None, 'toc': None,
+        'html-classes'      : {
             # Refer to typora exported html tags
+            # Note: 经测试发现, <ul>, <li>, <input> 标签设置 class 无效. 后面我会单独对
+            #       它们做修正.
+            'a': 'md-header-anchor',
         }
     })
+    html = html.replace(
+        '<li><input type="checkbox" class="task-list-item-checkbox" checked '
+        'disabled>',
+        '<li class="md-task-list-item task-list-item task-list-done"><input '
+        'type="checkbox"/>'
+    ).replace(
+        '<li><input type="checkbox" class="task-list-item-checkbox" disabled>',
+        '<li class="md-task-list-item task-list-item task-list-not-done">'
+        '<input type="checkbox"/>'
+    )
     
-    # Some modifications
-    if enable_checkbox is True:
-        # This allow us to switch states of checkboxes in browser.
-        html = re.sub(
-            r'<(input type="checkbox" class="task-list-item-checkbox"'
-            r'(?: checked)?) disabled>', r'<\1>', html
-        )
+    # return html  # TEST Return
     
-    # css: TODO
+    # --------------------------------------------------------------------------
     
-    return html
+    # For now the html has no stylesheet applied.
+    # We adjust it more compatible to Typora html format (like adding header and
+    # footer, etc.), and apply a Typora theme onto it.
+    
+    import textwrap  # https://google.github.io/styleguide/pyguide.html#310-strings
+    outframe = textwrap.dedent('''\
+        <!doctype html>
+        <html>
+            {__head__}
+            {__body__}
+        </html>
+    ''')
+    head = textwrap.dedent('''\
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width initial-scale=1'>
+            {__title__}
+            {__style__}
+        </head>
+    ''')
+    title = f'<title>{title}</title>'
+    style = f'<style type="text/css">' \
+            f'{read_file("../assets/github-typora.css")}</style>'
+    body = textwrap.dedent('''\
+        <body class='typora-export'>
+            <div id='write' class=''>
+                {__content__}
+            </div>
+        </body>
+    ''')
+    content = html
+    
+    body = body.format(__content__=content)
+    head = head.format(__title__=title, __style__=style)
+    outframe = outframe.format(__head__=head, __body__=body)
+    
+    return outframe
 
 
 if __name__ == '__main__':
     # main('../examples/demo.md', '../examples/demo_base64.md')
-    main('/Users/Likianta/Desktop/temp/test_20201126_000445.md')
+    main('/Users/Likianta/Desktop/documents/notebook/清单/博客文章.md',
+         '/Users/Likianta/Desktop/temp/test_content.html')
