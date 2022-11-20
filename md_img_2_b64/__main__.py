a7d0d543109b2c0625f530a3d6ff70c4cf134a26
likianta <likianta@foxmail.com>
2022-11-21 00:45:26 +0800

    Pre-normalize paths

--- md_img_2_b64/__main__.py
+++ md_img_2_b64/__main__.py
@@ -5,26 +5,32 @@ usage in cmd:
     python -m md_img_2_b64 md2html
     python -m md_img_2_b64 html2html
 """
-
 from argsense import cli
+from lk_utils.filesniff import normpath
 
 
 @cli.cmd()
 def md2md(file_i, file_o=''):
     from . import md_2_md
-    return md_2_md(file_i, file_o or file_i[:-3] + '.b64.md')
+    file_i = normpath(file_i)
+    file_o = normpath(file_o) if file_o else file_i[:-3] + '.b64.md'
+    return md_2_md(file_i, file_o)
 
 
 @cli.cmd()
 def md2html(file_i, file_o=''):
     from . import md_2_html
-    return md_2_html(file_i, file_o or file_i[:-3] + '.b64.html')
+    file_i = normpath(file_i)
+    file_o = normpath(file_o) if file_o else file_i[:-3] + '.b64.html'
+    return md_2_html(file_i, file_o)
 
 
 @cli.cmd()
 def html2html(file_i, file_o=''):
     from . import html_2_html
-    return html_2_html(file_i, file_o or file_i[:-5] + '.b64.html')
+    file_i = normpath(file_i)
+    file_o = normpath(file_o) if file_o else file_i[:-5] + '.b64.html'
+    return html_2_html(file_i, file_o)
 
 
 if __name__ == '__main__':
